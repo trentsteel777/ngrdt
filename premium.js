07f4b55a8f727f703df9b1a47637ff8d594c6e36
Trent Steel <trentsteel777@gmail.com>
2020-05-05 22:50:26 +0100

    Added in winston library for logging. NODE_ENV needs to be set in PROD

--- premium.js
+++ premium.js
@@ -5,45 +5,48 @@ const fs = require('fs')
 const axios = require('axios')
 const { EJDB2 } = require('ejdb2_node')
 const { thirdFridayOfNextMonth, today } = require('./src/utils.js')
-const logger = require('./src/logger.js')
+const { logger } = require('./src/logger.js')
 const sleep = require('sleep')
-var winston = require('winston')
 
 const symbols = fs.readFileSync('./src/resources/watchlist.txt').toString().split("\n")
+const symbolsLength = process.env.NODE_ENV !== 'production' ? 3 : symbols.length
 
 async function fetchAndSave() {
+  logger.info(`Opening database.`)
   const db = await EJDB2.open('optionchains.db', { truncate: true })
-
-  for (let i = 0; i < 3/* symbols.length */; i++) {
+  
+  for (let i = 0; i < symbolsLength; i++) {
     try {
       let apiUrl = `https://query2.finance.yahoo.com/v7/finance/options/${symbols[i]}?formatted=true&lang=en-US&region=US&date=${thirdFridayOfNextMonth}`
-  
+
+      logger.info(`{${apiUrl}}`)
       let response = await axios.get(apiUrl)
-  
+
       let key = response.data.optionChain.result[0].underlyingSymbol
       let json = response.data.optionChain.result[0]
-  
+
       await db.put(key, json, today)
 
       sleep.msleep(500)
     }
-    catch(e) {
-      console.log('Error: ' + e)
+    catch (e) {
+      logger.error('Error: ' + e)
     }
   }
 
-  for (let i = 0; i < 3/* symbols.length */; i++) {
+  for (let i = 0; i < symbolsLength; i++) {
     const q = db.createQuery('/*', symbols[i])
 
     for await (const doc of q.stream()) {
-        console.log(`Found ${doc.id} : ${doc.json.underlyingSymbol}`)
+      //logger.info(`Found ${doc.id} : ${doc.json.underlyingSymbol}`)
     }
   }
 
+  logger.info(`Closing database.`)
   await db.close()
 }
 
 fetchAndSave()
   .catch(e => {
-    console.log('Error: ' + e.message);
+    logger.error('Error: ' + e.message);
   })
--- src/logger.js
+++ src/logger.js
@@ -1,38 +1,56 @@
 const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, label, printf } = format;
+require('winston-daily-rotate-file')
+
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} ${level}: ${message}`; // Add back in if you want to name the logger: [${label}]
+});
 
 const logger = createLogger({
   level: 'info',
   format: format.combine(
-    format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss'
-    }),
-    format.errors({ stack: true }),
-    format.splat(),
-    format.json()
+    format.label({ label: 'def' }),
+    format.timestamp(),
+    myFormat
   ),
-  defaultMeta: { service: 'your-service-name' },
+  //defaultMeta: { service: 'your-service-name' },
   transports: [
-    //
     // - Write to all logs with level `info` and below to `quick-start-combined.log`.
     // - Write all logs error (and below) to `quick-start-error.log`.
-    //
-    new transports.File({ filename: 'quick-start-error.log', level: 'error' }),
-    new transports.File({ filename: 'quick-start-combined.log' }),
-    new transports.Console({ format: format.combine(format.colorize(),format.simple())})
+    new (transports.DailyRotateFile)({
+      level: 'error',
+      filename: 'error-%DATE%.log',
+      datePattern: 'YYYY-MM-DD',
+      utc: true,
+      dirname: 'logs',
+      zippedArchive: false,
+      maxSize: '20m',
+      maxFiles: '7d'
+    }),
+    new (transports.DailyRotateFile)({
+      level: 'info',
+      filename: 'application-%DATE%.log',
+      datePattern: 'YYYY-MM-DD',
+      utc: true,
+      dirname: 'logs',
+      zippedArchive: false,
+      maxSize: '20m',
+      maxFiles: '7d'
+    }),
   ]
 });
 
-//
 // If we're not in production then **ALSO** log to the `console`
 // with the colorized simple format.
-//
-/* if (process.env.NODE_ENV !== 'production') {
+// Make sure ENV Variable is set in PROD: vi ~/.bash_profile, then insert NODE_ENV=production
+if (process.env.NODE_ENV !== 'production') {
   logger.add(new transports.Console({
     format: format.combine(
       format.colorize(),
-      format.simple()
+      format.simple(),
+      myFormat
     )
   }));
-} */
-logger.info('Found s at s')
+}
+
 exports.logger = logger
