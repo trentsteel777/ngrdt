bc6d218ec9a2f1ab5439e33ba9b1420410725f89
Trent Steel <trentsteel777@gmail.com>
2020-05-07 00:42:01 +0100

    Removed using database and programmatically will determine needed options

--- premium.js
+++ premium.js
@@ -8,12 +8,14 @@ const { thirdFridayOfNextMonth, today } = require('./src/utils.js')
 const { logger } = require('./src/logger.js')
 const sleep = require('sleep')
 
+const googleRunSample = require('./src/insert-column.js')
+
 const symbols = fs.readFileSync('./src/resources/watchlist.txt').toString().split("\n")
 const symbolsLength = process.env.NODE_ENV !== 'production' ? 1 : symbols.length
 
 async function fetchAndSave() {
   logger.info(`Opening database.`)
-  const db = await EJDB2.open('optionchains.db', { truncate: true })
+  const db = await EJDB2.open('optionchains.db', { truncate: false })
   
   for (let i = 0; i < symbolsLength; i++) {
     try {
@@ -27,29 +29,57 @@ async function fetchAndSave() {
 
       await db.put(key, json, today)
 
-      sleep.msleep(500)
+      let price = json.quote.regularMarketPrice
+      let calls = json.options[0].calls
+      let puts = json.options[0].puts
+
+
+      sleep.msleep(1000)
     }
     catch (e) {
       logger.error('Error: ' + e)
     }
   }
 
-
-  for (let i = 0; i < symbolsLength; i++) {
-    try { //"contractSymbol": "A200619C00070000"
-      const q = db.createQuery('/options/0/calls/*/percentChange/[raw = 0] | /options/0/calls/*/percentChange/raw + /hasMiniOptions', symbols[i]) // [hasMiniOptions = :hasMiniOptions]
-  
-      for await (const doc of q.stream()) { // .setBoolean('hasMiniOptions', false) .setBoolean('inTheMoney', true)
-        logger.info(`Found ${doc} `)
-      }
-    }
-    catch(e) {
-      logger.error('Error reading DB: ' + e)
-    }
-  }
-
   logger.info(`Closing database.`)
   await db.close()
+}
+
+/*
+  https://github.com/trentsteel777/pygrdt/blob/master/analysisportal/views.py  
+   annotations = {
+      'marginOfSafety' : ( (F('optionChain__stock__price') - F('strike')) / F('optionChain__stock__price') ),
+      'returnOnOption' : F('bid') / ((F('strike') * D(0.1)) + F('bid')),
+      'marginPerContract' :  (((F('strike') * D(0.1)) + F('bid')) * sharesPerContract),
+      'exposurePerContract' : (F('optionChain__stock__price') * sharesPerContract),
+  } */
+function filterPuts(puts, stockPrice) {
+  let belowStrike = stockPrice * 0.85
+  let sharesPerContract = 100
+
+  let highPremiumPuts = []
+  for(let i = 0; i < puts.length; i++) {
+    let put = puts[i]
+    let putStrike = put.strike.raw
+    if(putStrike <= belowStrike) {
+      highPremiumPuts.push({
+        lastPrice : put.lastPrice.raw,
+        marginOfSafety : (stockPrice - putStrike) / stockPrice,
+        returnOnOption : lastPrice / ((putStrike * 0.1) + lastPrice),
+        marginPerContract : (((putStrike * 0.1) + lastPrice) * sharesPerContract),
+        exposurePerContract : (lastPrice * sharesPerContract)
+      })
+
+    }
+  }
+  return highPremiumPuts
+}
+
+function filterCalls(puts, price) {
+  let aboveStrike = price * 1.15
+  let sharesPerContract = 100
+
+
 }
 
 fetchAndSave()
---
+++ premium_db.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+'use strict'
+
+const fs = require('fs')
+const axios = require('axios')
+const { EJDB2 } = require('ejdb2_node')
+const { thirdFridayOfNextMonth, today } = require('./src/utils.js')
+const { logger } = require('./src/logger.js')
+const sleep = require('sleep')
+
+const symbols = fs.readFileSync('./src/resources/watchlist.txt').toString().split("\n")
+const symbolsLength = process.env.NODE_ENV !== 'production' ? 1 : symbols.length
+
+async function fetchAndSave() {
+  logger.info(`Opening database.`)
+  const db = await EJDB2.open('optionchains.db', { truncate: true })
+  
+  for (let i = 0; i < symbolsLength; i++) {
+    try {
+      let apiUrl = `https://query2.finance.yahoo.com/v7/finance/options/${symbols[i]}?formatted=true&lang=en-US&region=US&date=${thirdFridayOfNextMonth}`
+
+      logger.info(`{${apiUrl}}`)
+      let response = await axios.get(apiUrl)
+
+      let key = response.data.optionChain.result[0].underlyingSymbol
+      let json = response.data.optionChain.result[0]
+
+      await db.put(key, json, today)
+
+      sleep.msleep(500)
+    }
+    catch (e) {
+      logger.error('Error: ' + e)
+    }
+  }
+
+
+  for (let i = 0; i < symbolsLength; i++) {
+    try { //"contractSymbol": "A200619C00070000"
+
+    
+    const regularMarketPrice = await db.createQuery('/quote/regularMarketPrice | /quote/regularMarketPrice', symbols[i])
+
+    for await (const doc of regularMarketPrice.stream()) {
+      console.log(`Found ${doc}`);
+    }
+    const q = db.createQuery('/* | /options/0/calls/* + /quote/regularMarketPrice', symbols[i]) // [hasMiniOptions = :hasMiniOptions]
+  
+      for await (const doc of q.stream()) { // .setBoolean('hasMiniOptions', false) .setBoolean('inTheMoney', true)
+        logger.info(`Found ${doc} `)
+      }
+    }
+    catch(e) {
+      logger.error('Error reading DB: ' + e)
+    }
+  }
+
+  logger.info(`Closing database.`)
+  await db.close()
+}
+
+fetchAndSave()
+  .catch(e => {
+    logger.error('Error: ' + e.message);
+  })
