4d8ac827fb660d91c5411ef94616dd1133ed8bf4
trentsteel777 <trentsteel777@gmail.com>
2022-08-14 14:24:26 +0100

    Add nextSixFridaysUnixArr function in utils to get weekly expiration dates

--- src/utils.js
+++ src/utils.js
@@ -28,3 +28,30 @@ exports.today = (function () {
 exports.formatUnixDate = (function(unixEpochTime) {
     return moment.unix(unixEpochTime).format('YYYY-MM-DD')
 })
+
+// EXCEL: Unix date checker formula: =(G20/86400)+DATE(1970,1,1)
+exports.nextSixFridaysUnixArr = (function () { 
+    let thisFridayDate = moment()
+    .utc()
+    .endOf('isoweek')
+    .subtract(2, 'd')
+    .startOf('day')
+
+    if(thisFridayDate.isoWeekday() > 4) {
+        thisFridayDate.add(1, 'w')
+    }
+
+    let nextSixFridaysUnixArr = [thisFridayDate.unix()]     
+    for(let i = 0; i < 5; i++) {
+        thisFridayDate.add(1, 'w')
+        nextSixFridaysUnixArr.push(thisFridayDate.unix())
+    }
+
+    const index = nextSixFridaysUnixArr.indexOf(exports.thirdFridayOfNextMonth);
+    if (index > -1) { // only splice array when item is found
+        nextSixFridaysUnixArr.splice(index, 1); // 2nd parameter means remove one item only
+    }
+
+    return nextSixFridaysUnixArr
+})()
+
